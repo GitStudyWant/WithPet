351096c1df6e951f5b4ba30d7b188bf85a9b251e
GitStudyWant <134986954+GitStudyWant@users.noreply.github.com>
2023-06-01 12:29:29 +0900

    MemberDao 적용 -태영-

--- WithPet/src/main/java/com/project/withpet/member/model/dao/MemberDao.java
+++ WithPet/src/main/java/com/project/withpet/member/model/dao/MemberDao.java
@@ -18,6 +18,18 @@ public class MemberDao {
 		return sqlSession.selectOne("memberMapper.idCheck", checkId);
 	}
 	
+	public int nickCheck(SqlSession sqlSession, String checkNick) {
+		return sqlSession.selectOne("memberMapper.nickCheck", checkNick);
+	}
+	
+	public int phoneCheck(SqlSession sqlSession, String checkPhone) {
+		return sqlSession.selectOne("memberMapper.phoneCheck", checkPhone);
+	}
+	
+	public int emailCheck(SqlSession sqlSession, String checkEmail) {
+		return sqlSession.selectOne("memberMapper.emailCheck", checkEmail);
+	}
+	
 	
 	
 	
@@ -208,15 +220,5 @@ public class MemberDao {
 	
 	
 
-	public int nickCheck(SqlSession sqlSession, String checkNick) {
-		return sqlSession.selectOne("memberMapper.nickCheck", checkNick);
-	}
 	
-	public int phoneCheck(SqlSession sqlSession, String checkPhone) {
-		return sqlSession.selectOne("memberMapper.phoneCheck", checkPhone);
-	}
-	
-	public int emailCheck(SqlSession sqlSession, String checkEmail) {
-		return sqlSession.selectOne("memberMapper.emailCheck", checkEmail);
-	}
 }
