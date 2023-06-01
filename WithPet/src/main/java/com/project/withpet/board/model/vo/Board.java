81ed2122c40a7a497ee861dcb100986cae9dd9fb
pathzero00 <pathzero00@gmail.com>
2023-06-01 11:28:06 +0900

    board관련 -효기-
    vo 들 작성

--- WithPet/src/main/java/com/project/withpet/board/model/vo/Board.java
+++ WithPet/src/main/java/com/project/withpet/board/model/vo/Board.java
@@ -1,17 +1,21 @@
 package com.project.withpet.board.model.vo;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class Board {
 	
-	private int boardNo;// BOARD_NO	NUMBER
-	private String boardTitle;// BOARD_TITLE	VARCHAR2(100 BYTE)
-	private String boardWriter;// BOARD_WRITER	VARCHAR2(4000 BYTE)
-	private String boardContent;// BOARD_CONTENT	VARCHAR2(4000 BYTE)
-	private String originName;// ORIGIN_NAME	VARCHAR2(100 BYTE)
-	private String changeName;// CHANGE_NAME	VARCHAR2(100 BYTE)
-	private int count;// COUNT	NUMBER
-	private String createDate;// CREATE_DATE	DATE
-	private String status;// STATUS	VARCHAR2(1 BYTE)
+	private int boardNo;//BOARD_NO	NUMBER
+	private String boardTitle;//BOARD_TITLE	VARCHAR2(100 BYTE)
+	private String boardWriter;//BOARD_WRITER	VARCHAR2(50 BYTE)
+	private String boardContent;//BOARD_CONTENT	VARCHAR2(4000 BYTE)
+	private Date enrolldate;//ENROLLDATE	DATE
+	private int viewCount;//VIEW_COUNT	NUMBER
+	private String status;//STATUS	VARCHAR2(10 BYTE)
+	private int rating;//RATING	NUMBER
+	private int boardNum;//BOARD_NUM	NUMBER
+	
+	
 }
